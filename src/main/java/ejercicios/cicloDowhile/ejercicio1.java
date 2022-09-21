e7d8f96c750620c10542e49c2c7859b40fe8bbe8
Adryan Ynfante <109553913+adryanYnfante@users.noreply.github.com>
2022-09-21 11:21:40 -0500

    ejercicio do while

--- src/main/java/ejercicios/cicloDowhile/ejercicio1.java
+++ src/main/java/ejercicios/cicloDowhile/ejercicio1.java
@@ -1,18 +1,19 @@
 package ejercicios.cicloDowhile;
 
+import java.util.Scanner;
+
 public class ejercicio1 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        int i=20;
-        System.out.println("Inico del bucle");
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
         do
         {
-            i++;
-            System.out.println("Valor: " + i);
-        }while (i <= 10);
-        System.out.println("Fin bucle");
+            System.out.println("Ingrese un numero: ");
+            numero = sc.nextInt();
+        }
+        while(numero <= 500);
+    }
     }
 
-    //una variable con valor superior a la condición del bucle y podremos comprobar
-    // que primero entra en el bucle y después evalua el valor de la variable creada.
-}
